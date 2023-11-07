ba0d5918d06490926d23b0ffca6edc7728352083
blkluv <info@blkluv.org>
2023-11-06 19:06:15 -0500

    cta url added

--- components/About/components/Person/index.tsx
+++ components/About/components/Person/index.tsx
@@ -10,16 +10,19 @@ interface Props {
 
 const Person: React.FC<Props> = ({ person, styles, show, toggleShow }) => {
   return (
-    <div className={styles['person']}>
-      <img src={person.avatar} alt={person.name} />
-      <h4>{person.name}</h4>
-      <p>{person.role}</p>
-      <p>{person.summary}</p>
-      <p>{person.description}</p>
-      <a href={person.ctaURL} target="_blank" rel="noopener noreferrer">
-        Click here to learn more
-      </a>
-      <button onClick={toggleShow}>{show ? 'Show Less' : 'Show More'}</button>
+    <div className={styles['person-card']}>
+      <img className={styles['person-card-image']} src={person.avatar} alt={person.name} />
+      <div className={styles['person-card-content']}>
+        <h4>{person.name}</h4>
+        <div className={styles['person-card-content-role']}>
+          <p>{person.role}</p>
+        </div>
+        <p className={styles['description']}>{show ? person.description : person.summary}</p>
+        <a href={person.ctaURL} target="_blank" rel="noopener noreferrer"></a>
+        <p onClick={toggleShow} className={styles['show']}>
+          {show ? 'Hide' : 'Expand'}
+        </p>
+      </div>
     </div>
   );
 };
